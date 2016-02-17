1738b9eb23615206e643f66b3b96b6d36efb9070
SabbaKilam <abbas229@gmail.com>
2016-02-17 08:51:08 -0500

    I forget

--- main.js
+++ main.js
@@ -6,3 +6,11 @@
     Purpose: get used to phone gap
     Notes:
 */
+window.onload = resize;
+window.onresize = resize;
+function resize(){
+    var size = window.innerWidth;
+    var screen = window.screen.width;
+    var root = document.documentElement;
+    root.style.fontSize = ();
+}
