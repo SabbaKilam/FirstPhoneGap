c6e2aeebb310c3a51194d1c4d4c8ac376d6876ba
SabbaKilam <abbas229@gmail.com>
2016-02-22 08:51:29 -0500

    fixed banner

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 /*
     Author: Abbas Abdulmalik
-    Creation Date: 
+    Creation Date: February 15, 2016
     Title: First Phone gap app
-    Revised:
+    Revised: February 22, 2016
     Purpose: get used to phone gap
     Notes:
 */
+var $ = lib;
+
 window.onload = resize;
 window.onresize = resize;
+//---------------------------------
+$("#menu").hover(overMenu, offMenu);
+function overMenu(){
+    $("#menu").css("color","white");
+}
+function offMenu(){
+    $("#menu").css("color","lightblue");    
+}
+//---------------------------------
 function resize(){
     var size = window.innerWidth;
     var screen = window.screen.width;
     var root = document.documentElement;
-    root.style.fontSize = ();
+    root.style.fontSize = (15 + size/200) + "px";
 }
+
+
+
+
+
+
